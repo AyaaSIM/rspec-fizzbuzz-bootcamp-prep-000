d86e4e714452dc45185e8a386b485bec14e1bf25
Ayaa <ayaa1siddig5@gmail.com>
2018-08-12 04:01:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  fizz_3=fizzbuzz(int) % 3 == 0
-  if fizz_3 
-    puts "Fizz"
-  end 
-end 
+describe fizzbuzz do
