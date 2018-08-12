510915de568c510980ea6ac7e66b3e1ebd435d9b
Ayaa <ayaa1siddig5@gmail.com>
2018-08-12 03:58:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  fizz_3=fizzbuzz % 3 == 0
+  fizz_3=fizzbuzz(int) % 3 == 0
   if fizz_3 
     puts "Fizz"
   end 
